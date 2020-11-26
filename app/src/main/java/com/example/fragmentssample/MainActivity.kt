ae2f29ac3ee0f66ae401308c904ed07795b81c9c
edidiongaligbe <edidiong.aligbe@gmail.com>
2020-11-26 09:39:22 +0100

    Android app with kotlin that uses fragments and navigation - menu and navigation drawer that slides out from the side of the screen.

---
+++ app/src/main/java/com/example/fragmentssample/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.fragmentssample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.fragmentssample.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //initialize drawerLayout after the binding variable has been initialized
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+//Add the drawerLayout as the third parameter to the setupActionBarWithNavController() method+
+        NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+
+}
